04382a45328293ef912e2322796b6cf41cfe955b
benjaminramovic <benjaminramovic02@gmail.com>
2025-01-06 00:47:14 +0100

    finished-guests

--- Micro.RoomLink.APIGateway/Controllers/GuestController.cs
+++ Micro.RoomLink.APIGateway/Controllers/GuestController.cs
@@ -1,5 +1,8 @@
+using System.Text;
+using Micro.RoomLink.APIGateway.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace Micro.RoomLink.APIGateway.Controllers;
 
@@ -35,4 +38,87 @@ public class GuestController : ControllerBase
             return StatusCode(500, e.Message);
         }
     }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetGuest([FromRoute] int id)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync(_urls.Guests + "api/guests/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return Content(responseContent, response.Content.Headers.ContentType?.ToString() ?? "application/json");
+            }
+            return StatusCode((int)response.StatusCode);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Guest guest)
+    {
+        try
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(guest),Encoding.UTF8, @"application/json");
+            var response = await _httpClient.PostAsync(_urls.Guests + "api/guests", content);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return Content(responseContent, response.Content.Headers.ContentType?.ToString() ?? "application/json");
+            }
+
+            return StatusCode((int)response.StatusCode);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute]int id)
+    {
+        try
+        {
+            var response = await _httpClient.DeleteAsync(_urls.Guests + "api/guests/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return Content(responseContent, response.Content.Headers.ContentType?.ToString() ?? "application/json");
+            }
+            return StatusCode((int)response.StatusCode);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Guest guest)
+    {
+        try
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(guest), Encoding.UTF8, @"application/json");
+            var response = await _httpClient.PutAsync(_urls.Guests + "api/guests/" + id, content);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return Content(responseContent, response.Content.Headers.ContentType?.ToString() ?? "application/json");
+            }
+
+            return StatusCode((int)response.StatusCode);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+
+   
 }
--- Micro.RoomLink.APIGateway/Models/Guest.cs
+++ Micro.RoomLink.APIGateway/Models/Guest.cs
@@ -3,9 +3,9 @@ namespace Micro.RoomLink.APIGateway.Models;
 
 public class Guest
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public DateTime DateOfBirth { get; set; }
+    public int id { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string email { get; set; }
+    public DateTime dateOfBirth { get; set; }
 }
